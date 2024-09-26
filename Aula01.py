951eead6ab1f588e2d600bdd0f7f0e242bd7c7e5
JoseRCL1981 <jribamarcl@hotmail.com>
2024-09-25 21:31:48 -0300

    Add files via upload

---
+++ Aula01.py
@@ -0,0 +1,8 @@
+print('O que é pythom')
+print('Python é uma das principais e mais populares linguagens de programação em todo o mundo\n')
+
+print('Qual programa (IDE) executa o arquivo .py')
+print('O programa que executa o python é o VScode\n')
+
+print('Qual Linguagem de programação Python substituiu?')
+print('A linguagem que o python substituiu foi a ABC\n')
